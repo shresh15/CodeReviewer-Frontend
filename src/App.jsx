fe792479a3265cd6c5d295a059ce78ac455fbb9d
shrestha <shresthachakrabortykgp@gmail.com>
2025-02-18 21:15:46 +0530

    UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,53 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import "prismjs/themes/prism-tomorrow.css";
+import prism from "prismjs";
+import Editor from "react-simple-code-editor";
+import axios from "axios";
 function App() {
-  const [count, setCount] = useState(0);
+  const [code, setCode] = useState(`function sum() {
+    return 1 + 1;
+  }`);
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
+
+  async function reviewCode() {
+    const response = await axios.post("htt://localhost:3000/ai/get-review", {
+      code,
+    });
+    console.log(response.data);
+  }
 
   return (
-    <div className="w-full h-screen bg-linear-to-r/srgb from-indigo-300 to-teal-400 flex items-center justify-center">
+    <div className="w-full h-screen bg-gradient-to-r from-violet-900 to-black flex items-center justify-center">
       <main className="w-3/4 h-5/6 flex">
         {/* Left Section */}
-        <div className="w-1/2 h-full bg-gray-800 p-6 flex flex-col justify-between">
-          <div className="code bg-blue-300 p-4 rounded-lg shadow-md">
-            Code Section
+        <div className="left w-1/2 h-full bg-gray-800 p-6 flex flex-col justify-between">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={(code) => setCode(code)}
+              highlight={(code) =>
+                prism.highlight(code, prism.languages.javascript, "javascript")
+              }
+              padding={10}
+              style={{
+                fontSize: 12,
+                height: "100%",
+                width: "100%",
+                backgroundColor: "transparent", // Optional: Removes default background
+                color: "#ffffff", // Ensures text is visible on dark backgrounds
+              }}
+            />
           </div>
-          <div className="review bg-blue-400 p-4 rounded-lg shadow-md mt-4">
-            Review Section
+          <div onClick={reviewCode}>
+            <div className="review bg-blue-400 p-4 rounded-lg shadow-md mt-4">
+              Review
+            </div>
           </div>
-          <button
-            className="mt-4 bg-white text-blue-500 px-4 py-2 rounded-lg hover:bg-gray-200 transition"
-            onClick={() => setCount(count + 1)}
-          >
-            Click Me ({count})
-          </button>
+          {/* <pre className="!bg-transparent !p-0 !shadow-none">
+            <code className="language-javascript"></code>
+          </pre> */}
         </div>
 
         {/* Right Section */}
